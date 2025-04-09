eb1b0ad14cb227ddb8ad5ea9e31d02d23697c58c
Elyes <elyes.bk@orange.fr>
2025-04-09 18:43:29 +0200

    test connexion admin

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -77,7 +77,7 @@ export async function getServerSideProps(context){
     if(!session){
       return{
         redirect:{
-          destination: 'api/auth/signin',
+          destination: '/api/auth/signin?callbackUrl=/admin',
           permanent: false,
         }
       }
